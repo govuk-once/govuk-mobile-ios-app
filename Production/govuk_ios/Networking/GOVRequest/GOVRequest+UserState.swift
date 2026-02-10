dbb320b854f7a613990220d9cf7843723331449f
Krzysztof.Skierniewski <k.skierniewski@gmail.com>
2026-02-10 14:10:14 +0000

    added UserState arrangers and fixed indentation in UserState GOVRequest

--- Production/govuk_ios/Networking/GOVRequest/GOVRequest+UserState.swift
+++ Production/govuk_ios/Networking/GOVRequest/GOVRequest+UserState.swift
@@ -2,26 +2,35 @@ import Foundation
 import GovKit
 
 extension GOVRequest {
+    private static let userPath = "/app/v1/user"
+
     static func getUserState(accessToken: String?) -> GOVRequest {
         let accessToken = accessToken ?? ""
         return GOVRequest(
-            urlPath: "/app/v1/user",
+            urlPath: userPath,
             method: .get,
             bodyParameters: nil,
             queryParameters: nil,
-            additionalHeaders: ["Authorization": "Bearer \(accessToken)"]
+            additionalHeaders: [
+                "Authorization": "Bearer \(accessToken)"
+            ]
         )
     }
 
     static func setNotificationsConsent(accepted: Bool,
                                         accessToken: String?) -> GOVRequest {
         let accessToken = accessToken ?? ""
-        return GOVRequest(urlPath: "/app/v1/user",
-                          method: .patch,
-                          bodyParameters: ["notificationsConsented": accepted],
-                          queryParameters: nil,
-                          additionalHeaders: ["Content-Type": "application/json",
-                                              "Authorization": "Bearer \(accessToken)"]
+        return GOVRequest(
+            urlPath: userPath,
+            method: .patch,
+            bodyParameters: [
+                "notificationsConsented": accepted
+            ],
+            queryParameters: nil,
+            additionalHeaders: [
+                "Content-Type": "application/json",
+                "Authorization": "Bearer \(accessToken)"
+            ]
         )
     }
 }
---
+++ Tests/govuk_ios/govuk_ios_unit_tests/Arrangers/Model/UserState+Arrangers.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+@testable import govuk_ios
+
+extension UserState {
+    static var arrange: UserState {
+        .arrange()
+    }
+
+    static func arrange(notificationId: String = "test_id") -> UserState {
+        .init(
+            notificationId: notificationId,
+            preferences: UserPreferences(
+                notifications: ConsentPreference(
+                    consentStatus: .unknown,
+                    updatedAt: Date()
+                )
+            )
+        )
+    }
+}
+
