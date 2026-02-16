6a662d1f3c29448655547d2c9381c76f361c0a48
Joshua Dubey <josh.dubey1@digital.cabinet-office.gov.uk>
2026-02-16 16:55:24 +0000

    Add tests

--- Tests/govuk_ios/shared/Mocks/Services/MockTermsAndConditionsService.swift
+++ Tests/govuk_ios/shared/Mocks/Services/MockTermsAndConditionsService.swift
@@ -1,2 +1,30 @@
-//
+import Foundation
 
+@testable import govuk_ios
+
+final class MockTermsAndConditionsService: TermsAndConditionsServiceInterface {
+    var _stubbedTermsAcceptanceIsValid: Bool = false
+    var termsAcceptanceIsValid: Bool {
+        _stubbedTermsAcceptanceIsValid
+    }
+
+    var _stubbedTermsAndConditionsURL: URL = URL(string: "https://www.example.com")!
+    var termsAndConditionsURL: URL {
+        _stubbedTermsAndConditionsURL
+    }
+
+    var _stubbedHasUpdatedTerms: Bool = false
+    var hasUpdatedTerms: Bool {
+        _stubbedHasUpdatedTerms
+    }
+
+    var _didSaveAcceptanceDate: Bool = false
+    func saveAcceptanceDate() {
+        _didSaveAcceptanceDate = true
+    }
+
+    var _didResetAcceptanceDate: Bool = false
+    func resetAcceptanceDate() {
+        _didResetAcceptanceDate = true
+    }
+}
