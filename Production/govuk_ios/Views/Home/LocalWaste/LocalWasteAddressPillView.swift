4450d9a985259b51bd9a170a578390afdcac4d78
peter-major-apadmi <69640374+peter-major-apadmi@users.noreply.github.com>
2026-03-27 09:37:37 +0000

    Use updated location icon

--- Production/govuk_ios/Views/Home/LocalWaste/LocalWasteAddressPillView.swift
+++ Production/govuk_ios/Views/Home/LocalWaste/LocalWasteAddressPillView.swift
@@ -15,10 +15,12 @@ struct LocalWasteAddressPillView: View {
     var body: some View {
         HStack(alignment: .center, spacing: 0) {
             Image(.localLocationIcon)
-                .padding(8)
+                .padding(.leading, 16)
+                .padding(.trailing, 8)
+                .padding(.vertical, 8)
             Text(addressString)
                 .padding(.vertical, 8)
-                .padding(.trailing, 8)
+                .padding(.trailing, 16)
                 .font(Font.govUK.footnote)
                 .foregroundColor(Color(UIColor.govUK.text.header))
                 .fixedSize(horizontal: false, vertical: true)
