41d7344ee2f1a0bbea472eefabb382ff261af7bd
raphael olumofe <raphaelo@hotmail.co.uk>
2026-04-13 10:36:21 +0100

    refactor

--- Production/govuk_ios/SwiftUIViews/CancelButton.swift
+++ Production/govuk_ios/SwiftUIViews/CancelButton.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct CancelButton: View {
     let action: () -> Void
     var body: some View {
-        if #available(iOS 26.0, *) {
             Button(
                 action: {
                     action()
@@ -14,20 +13,12 @@ struct CancelButton: View {
                         )
                         .font(Font.govUK.subheadlineSemibold)
                 }
-            ).buttonStyle(.glass)
-        } else {
-            Button(
-                action: {
-                    action()
-                }, label: {
-                    Text(String.common.localized("cancel"))
-                        .foregroundColor(
-                            Color(UIColor.govUK.text.linkSecondary)
-                        )
-                        .font(Font.govUK.subheadlineSemibold)
+            ).applyStyle {
+                if #available(iOS 26.0, *) {
+                    $0.buttonStyle(.glass)
+                } else {
+                    $0
                 }
-            )
-        }
+            }
     }
 }
-
