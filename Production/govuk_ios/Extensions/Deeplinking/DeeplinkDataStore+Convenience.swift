99d1eceee0856c978da454593943034917f2a3df
Matt Malone <matthew.malone@digital.cabinet-office.gov.uk>
2026-05-26 20:45:21 +0100

    fix: NOT-259 remove deeplink to notification centre list

--- Production/govuk_ios/Extensions/Deeplinking/DeeplinkDataStore+Convenience.swift
+++ Production/govuk_ios/Extensions/Deeplinking/DeeplinkDataStore+Convenience.swift
@@ -11,7 +11,6 @@ extension DeeplinkDataStore {
                 SearchDeeplinkRoute(coordinatorBuilder: coordinatorBuilder),
                 EditTopicsDeeplinkRoute(coordinatorBuilder: coordinatorBuilder),
                 RecentActivityDeeplinkRoute(coordinatorBuilder: coordinatorBuilder),
-                NotificationCentreDeeplinkRoute(coordinatorBuilder: coordinatorBuilder),
                 NotificationCentreDetailDeeplinkRoute(coordinatorBuilder: coordinatorBuilder)
             ],
             root: root
@@ -37,15 +36,4 @@ extension DeeplinkDataStore {
             root: root
         )
     }
-
-//    static func notificationCentre(coordinatorBuilder: CoordinatorBuilder,
-//                                   root: UIViewController) -> DeeplinkDataStore {
-//        DeeplinkDataStore(
-//            routes: [
-//                NotificationCentreDeeplinkRoute(coordinatorBuilder: coordinatorBuilder),
-//                NotificationCentreDetailDeeplinkRoute(coordinatorBuilder: coordinatorBuilder)
-//            ],
-//            root: root
-//        )
-//    }
 }
