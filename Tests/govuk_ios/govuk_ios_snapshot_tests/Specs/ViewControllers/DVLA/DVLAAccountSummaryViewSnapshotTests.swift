7c699cd72b1259f74083e5487c01bb9f5edbfd30
raphael olumofe <raphaelo@hotmail.co.uk>
2026-06-11 09:30:49 +0100

    added segmented view controller

--- Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/DVLA/DVLAAccountSummaryViewSnapshotTests.swift
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/DVLA/DVLAAccountSummaryViewSnapshotTests.swift
@@ -6,7 +6,7 @@ import GovKit
 @testable import govuk_ios
 
 @MainActor class DVLAAccountSummaryViewSnapshotTests: SnapshotTestCase {
-    func test_vehiclesAndLicenceView_light_rendersCorrectly() {
+    func test_vehicleTabSelected_light_rendersCorrectly() {
         let mockVehiclesViewModel = MockVehiclesViewModel(
             viewState: .loaded(
                 vehicles: [
@@ -19,10 +19,100 @@ import GovKit
                 licence: DrivingLicenceSummaryViewModel(driverSummary: .arrange)
             )
         )
-        let viewModel = MockDVLAAccountSummaryViewModel(
+        let viewModel = DVLAAccountSummaryViewModel(
             vehiclesViewModel: mockVehiclesViewModel,
             licenceViewModel: mockLicenceViewModel
         )
+        viewModel.selectedScreen = .vehicles
+        let view = DVLAAccountSummaryView(viewModel: viewModel)
+        let hostingViewController =  HostingViewController(
+            rootView: view
+        )
+        hostingViewController.view.backgroundColor = .govUK.fills.surfaceBackground
+        VerifySnapshotInNavigationController(
+            viewController: hostingViewController,
+            mode: .light
+        )
+    }
+
+    func test_vehicleTabSelected_dark_rendersCorrectly() {
+        let mockVehiclesViewModel = MockVehiclesViewModel(
+            viewState: .loaded(
+                vehicles: [
+                    VehicleSummaryViewModel(vehicle: .arrange)
+                ]
+            )
+        )
+        let mockLicenceViewModel = MockDrivingLicenceViewModel(
+            viewState: .loaded(
+                licence: DrivingLicenceSummaryViewModel(driverSummary: .arrange)
+            )
+        )
+        let viewModel = DVLAAccountSummaryViewModel(
+            vehiclesViewModel: mockVehiclesViewModel,
+            licenceViewModel: mockLicenceViewModel
+        )
+        viewModel.selectedScreen = .vehicles
+        let view = DVLAAccountSummaryView(viewModel: viewModel)
+        let hostingViewController =  HostingViewController(
+            rootView: view
+        )
+        hostingViewController.view.backgroundColor = .govUK.fills.surfaceBackground
+        VerifySnapshotInNavigationController(
+            viewController: hostingViewController,
+            mode: .dark
+        )
+    }
+
+    func test_licenceView_dark_rendersCorrectly() {
+        let mockVehiclesViewModel = MockVehiclesViewModel(
+            viewState: .loaded(
+                vehicles: [
+                    VehicleSummaryViewModel(vehicle: .arrange)
+                ]
+            )
+        )
+        let mockLicenceViewModel = MockDrivingLicenceViewModel(
+            viewState: .loaded(
+                licence: DrivingLicenceSummaryViewModel(driverSummary: .arrange)
+            )
+        )
+
+        let viewModel = DVLAAccountSummaryViewModel(
+            vehiclesViewModel: mockVehiclesViewModel,
+            licenceViewModel: mockLicenceViewModel
+        )
+        viewModel.selectedScreen = .drivingLicence
+        let view = DVLAAccountSummaryView(viewModel: viewModel)
+        let hostingViewController =  HostingViewController(
+            rootView: view
+        )
+        hostingViewController.view.backgroundColor = .govUK.fills.surfaceBackground
+        VerifySnapshotInNavigationController(
+            viewController: hostingViewController,
+            mode: .dark
+        )
+    }
+
+    func test_licenceView_light_rendersCorrectly() {
+        let mockVehiclesViewModel = MockVehiclesViewModel(
+            viewState: .loaded(
+                vehicles: [
+                    VehicleSummaryViewModel(vehicle: .arrange)
+                ]
+            )
+        )
+        let mockLicenceViewModel = MockDrivingLicenceViewModel(
+            viewState: .loaded(
+                licence: DrivingLicenceSummaryViewModel(driverSummary: .arrange)
+            )
+        )
+
+        let viewModel = DVLAAccountSummaryViewModel(
+            vehiclesViewModel: mockVehiclesViewModel,
+            licenceViewModel: mockLicenceViewModel
+        )
+        viewModel.selectedScreen = .drivingLicence
         let view = DVLAAccountSummaryView(viewModel: viewModel)
         let hostingViewController =  HostingViewController(
             rootView: view
