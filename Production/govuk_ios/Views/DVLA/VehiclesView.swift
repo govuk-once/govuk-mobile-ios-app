0397a79900bb9c8db28b1b3393551dc92831ecfb
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2026-06-18 09:59:11 +0100

    Update accessibility and small refactor
    Adds link trait and "opens in web browser" hint.

--- Production/govuk_ios/Views/DVLA/VehiclesView.swift
+++ Production/govuk_ios/Views/DVLA/VehiclesView.swift
@@ -39,7 +39,6 @@ struct VehiclesView: View {
         .padding(.horizontal, 16)
     }
 
-    @ViewBuilder
     private func makeVehiclesView(for vehicleViewModels: [VehicleSummaryViewModel]) -> some View {
         LazyVStack(spacing: 16) {
             ForEach(vehicleViewModels) {
@@ -48,67 +47,66 @@ struct VehiclesView: View {
             .background(Color(UIColor.govUK.fills.surfaceList))
             .clipShape(RoundedRectangle(cornerRadius: 10))
 
-            if vehicleViewModels.isEmpty {
+            addVehiclesCard(large: vehicleViewModels.isEmpty)
+        }
+        .padding(.horizontal, 16)
+    }
+
+    private func addVehiclesCard(large: Bool) -> some View {
+        Button {
+            viewModel.addNewVehiclesAction()
+        } label: {
+            if large {
                 addVehiclesLargeCard
             } else {
                 addVehiclesSmallCard
             }
         }
-        .padding(.horizontal, 16)
+        .accessibilityElement(children: .combine)
+        .accessibilityRemoveTraits(.isButton)
+        .accessibilityAddTraits(.isLink)
+        .accessibilityHint(String.common.localized("openWebLinkHint"))
     }
 
     private var addVehiclesLargeCard: some View {
-        Button {
-            viewModel.addNewVehiclesAction()
-        } label: {
-            VStack(alignment: .center, spacing: 16) {
-                Image(systemName: "plus.circle")
-                    .font(.govUK.largeTitle.weight(.light))
-                    .foregroundColor(
-                        Color(.govUK.text.iconTertiary)
-                    )
-                    .font(.title)
-                    .accessibilityHidden(true)
-                Text(String.dvla.localized("dvlaAddNewVehiclesTitle"))
-                    .multilineTextAlignment(.center)
-                    .font(.govUK.body)
-                    .foregroundColor(
-                        Color(.govUK.text.primary))
-                    .padding(.horizontal, 32)
-            }
-            .frame(maxWidth: .infinity)
-            .padding(.vertical, 52)
-            .background {
-                Color(.govUK.fills.surfaceList)
-            }
-            .roundedBorder(borderColor: .clear)
-            .accessibilityElement(children: .combine)
+        VStack(alignment: .center, spacing: 16) {
+            Image(systemName: "plus.circle")
+                .font(.govUK.largeTitle.weight(.light))
+                .foregroundColor(
+                    Color(.govUK.text.iconTertiary)
+                )
+                .font(.title)
+                .accessibilityHidden(true)
+            Text(String.dvla.localized("dvlaAddNewVehiclesTitle"))
+                .multilineTextAlignment(.center)
+                .font(.govUK.body)
+                .foregroundColor(
+                    Color(.govUK.text.primary))
+                .padding(.horizontal, 32)
         }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 52)
+        .background(Color(.govUK.fills.surfaceList))
+        .roundedBorder(borderColor: .clear)
     }
 
     private var addVehiclesSmallCard: some View {
-        Button(action: {
-            viewModel.addNewVehiclesAction()
-        },
-        label: {
-            HStack {
-                Text(String.dvla.localized("dvlaAddNewVehicleTitle"))
-                    .font(.govUK.body)
-                    .foregroundStyle(Color(.govUK.text.primary))
-                    .multilineTextAlignment(.leading)
-                Spacer()
-                Image(systemName: "plus.circle")
-                    .font(.govUK.title3Semibold)
-                    .frame(minWidth: Self.iconSize, minHeight: Self.iconSize)
-                    .foregroundStyle(Color(.govUK.text.iconTertiary))
-                    .accessibilityHidden(true)
-            }
-        })
+        HStack {
+            Text(String.dvla.localized("dvlaAddNewVehicleTitle"))
+                .font(.govUK.body)
+                .foregroundStyle(Color(.govUK.text.primary))
+                .multilineTextAlignment(.leading)
+            Spacer()
+            Image(systemName: "plus.circle")
+                .font(.govUK.title3Semibold)
+                .frame(minWidth: Self.iconSize, minHeight: Self.iconSize)
+                .foregroundStyle(Color(.govUK.text.iconTertiary))
+                .accessibilityHidden(true)
+        }
         .padding(.horizontal)
         .padding(.vertical, 12)
         .background(Color(.govUK.fills.surfaceCardDefault))
         .roundedBorder(borderColor: .clear)
-        .accessibilityElement(children: .combine)
     }
 
     private func makeErrorView(for errorViewModel: AppErrorViewModel) -> some View {
