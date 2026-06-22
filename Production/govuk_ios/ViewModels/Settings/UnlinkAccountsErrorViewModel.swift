c5854e9ee78a71fc68357dc0ac05908bdb230886
raphael olumofe <raphaelo@hotmail.co.uk>
2026-06-22 15:18:10 +0100

    string localisation

--- Production/govuk_ios/ViewModels/Settings/UnlinkAccountsErrorViewModel.swift
+++ Production/govuk_ios/ViewModels/Settings/UnlinkAccountsErrorViewModel.swift
@@ -4,15 +4,25 @@ import GovKitUI
 
 final class UnlinkAccountsErrorViewModel {
     private let unlinkErrorAction: () -> Void
-    let title = "There’s a problem"
-    let description = "We could not remove your driver and vehicles account. Try again later."
+
+    let title = String(
+        localized: .Settings.yourAccountsUnlinkingErrorTitle
+    )
+    let description = String(
+        localized: .Settings.yourAccountsUnlinkingErrorDescription
+    )
+    let errorViewDescription = String(
+        localized: .Settings.yourAccountsErrorViewDesc
+    )
+    let buttonTitle = String(
+        localized: .Settings.yourAccountsUnlinkingErrorButtonTitle
+    )
 
     init(unlinkErrorAction: @escaping () -> Void) {
         self.unlinkErrorAction = unlinkErrorAction
     }
 
     var primaryButtonViewModel: GOVUKButton.ButtonViewModel {
-        let buttonTitle = "Go back to your accounts"
         return GOVUKButton.ButtonViewModel(
             localisedTitle: buttonTitle,
             action: { [weak self] in
