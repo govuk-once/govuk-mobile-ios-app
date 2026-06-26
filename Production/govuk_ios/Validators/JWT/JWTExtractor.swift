f61f88fdcae1213fb1b31cbb65a8a9ecd7cd9db3
Tom Bates <thomas.bates@digital.cabinet-office.gov.uk>
2026-06-26 14:35:59 +0100

    Fixed unused parameter error in JWTExtractor

--- Production/govuk_ios/Validators/JWT/JWTExtractor.swift
+++ Production/govuk_ios/Validators/JWT/JWTExtractor.swift
@@ -2,13 +2,13 @@ import Foundation
 import JWTKit
 
 protocol JWTExtractorInterface {
-    func extract<T: JWTPayload>(jwt: String, as type: T.Type) async throws -> T
+    func extract<T: JWTPayload>(jwt: String) async throws -> T
 }
 
 struct JWTExtractor: JWTExtractorInterface {
     let signers = JWTKeyCollection()
 
-    func extract<T: JWTPayload>(jwt: String, as type: T.Type) async throws -> T {
-        return try await signers.unverified(jwt, as: T.self)
+    func extract<T: JWTPayload>(jwt: String) async throws -> T {
+        try await signers.unverified(jwt, as: T.self)
     }
 }
