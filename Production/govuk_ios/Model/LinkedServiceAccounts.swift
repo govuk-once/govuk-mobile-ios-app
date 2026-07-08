c150d52488ec3ea33814172cd2135006722bb947
kris-skierniewski <64841676+kris-skierniewski@users.noreply.github.com>
2026-07-08 15:50:23 +0100

    fix for unrecognised linked service accounts being returned from flex (#234)

--- Production/govuk_ios/Model/LinkedServiceAccounts.swift
+++ Production/govuk_ios/Model/LinkedServiceAccounts.swift
@@ -2,4 +2,18 @@ import Foundation
 
 struct LinkedServiceAccounts: Codable {
     let services: [ServiceAccountType]
+
+    enum CodingKeys: String, CodingKey {
+        case services
+    }
+
+    init(services: [ServiceAccountType]) {
+        self.services = services
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let servicesStrings = try container.decode([String].self, forKey: .services)
+        self.services = servicesStrings.compactMap { ServiceAccountType(rawValue: $0) }
+    }
 }
