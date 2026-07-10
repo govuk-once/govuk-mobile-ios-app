3b7814b891f9fdcee9d31ad2c09fecbd681ec1e5
raphael olumofe <raphaelo@hotmail.co.uk>
2026-07-10 15:17:04 +0100

    commented out tests to see which one causes the ci to break

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewModels/DVLA/DrivingLicenceSummaryViewModelTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewModels/DVLA/DrivingLicenceSummaryViewModelTests.swift
@@ -5,99 +5,99 @@ import Testing
 @testable import GovKit
 
 struct DrivingLicenceSummaryViewModelTests {
-    @Test
-    func init_formatsLicenceNumberCorrectly() {
-        let mockDrivingLicence = DrivingLicence.arrange(licenceNumber: "ABC123AE")
-        let sut = DrivingLicenceSummaryViewModel(
-            drivingLicence: mockDrivingLicence,
-            statusBuilder: MockLicenceStatusViewModelBuilder(),
-            openURLAction: { _, _ in },
-            menuSelectionAction: {_ in },
-            copyToClipboardAction: {_ in },
-            analyticsService: MockAnalyticsService()
-        )
-        #expect(sut.licenceNumber == "ABC123AE")
-    }
+//    @Test
+//    func init_formatsLicenceNumberCorrectly() {
+//        let mockDrivingLicence = DrivingLicence.arrange(licenceNumber: "ABC123AE")
+//        let sut = DrivingLicenceSummaryViewModel(
+//            drivingLicence: mockDrivingLicence,
+//            statusBuilder: MockLicenceStatusViewModelBuilder(),
+//            openURLAction: { _, _ in },
+//            menuSelectionAction: {_ in },
+//            copyToClipboardAction: {_ in },
+//            analyticsService: MockAnalyticsService()
+//        )
+//        #expect(sut.licenceNumber == "ABC123AE")
+//    }
 
-    @Test
-        func openUrl_changeAddresss_tracksExpectedNavigationEvent() {
-            let mockDrivingLicence = DrivingLicence.arrange(licenceNumber: "ABC123AE")
-            let mockAnalytics = MockAnalyticsService()
-            let sut = DrivingLicenceSummaryViewModel(
-                drivingLicence: mockDrivingLicence,
-                statusBuilder: MockLicenceStatusViewModelBuilder(),
-                openURLAction: { _,_ in},
-                menuSelectionAction: { _ in },
-                copyToClipboardAction: { _ in},
-                analyticsService: mockAnalytics
-            )
+//    @Test
+//        func openUrl_changeAddresss_tracksExpectedNavigationEvent() {
+//            let mockDrivingLicence = DrivingLicence.arrange(licenceNumber: "ABC123AE")
+//            let mockAnalytics = MockAnalyticsService()
+//            let sut = DrivingLicenceSummaryViewModel(
+//                drivingLicence: mockDrivingLicence,
+//                statusBuilder: MockLicenceStatusViewModelBuilder(),
+//                openURLAction: { _,_ in},
+//                menuSelectionAction: { _ in },
+//                copyToClipboardAction: { _ in},
+//                analyticsService: mockAnalytics
+//            )
+//    
+//            sut.openUrl(options: .changeAddress)
+//    
+//            #expect(mockAnalytics._trackedEvents.count == 1)
+//    
+//            guard let event = mockAnalytics._trackedEvents.first else {
+//                return
+//            }
+//            #expect(event.name == "Navigation")
+//            #expect(event.params?["text"] as? String == "Change address")
+//            #expect(event.params?["type"] as? String == "Menu")
+//            #expect(event.params?["external"] as? Bool == true)
+//        }
     
-            sut.openUrl(options: .changeAddress)
-    
-            #expect(mockAnalytics._trackedEvents.count == 1)
-    
-            guard let event = mockAnalytics._trackedEvents.first else {
-                return
-            }
-            #expect(event.name == "Navigation")
-            #expect(event.params?["text"] as? String == "Change address")
-            #expect(event.params?["type"] as? String == "Menu")
-            #expect(event.params?["external"] as? Bool == true)
-        }
-    
-        @Test
-        func openUrl_replaceLicence_tracksExpectedNavigationEvent() {
-            let mockDrivingLicence = DrivingLicence.arrange(licenceNumber: "ABC123AE")
-            let mockAnalytics = MockAnalyticsService()
-            let sut = DrivingLicenceSummaryViewModel(
-                drivingLicence: mockDrivingLicence,
-                statusBuilder: MockLicenceStatusViewModelBuilder(),
-                openURLAction: { _,_ in},
-                menuSelectionAction: { _ in },
-                copyToClipboardAction: { _ in },
-                analyticsService: mockAnalytics
-            )
-    
-            sut.openUrl(options: .replaceLicence)
-    
-            #expect(mockAnalytics._trackedEvents.count == 1)
-    
-            guard let event = mockAnalytics._trackedEvents.first else {
-                return
-            }
-    
-            #expect(event.name == "Navigation")
-            #expect(event.params?["text"] as? String == "Replace licence")
-            #expect(event.params?["type"] as? String == "Menu")
-            #expect(event.params?["external"] as? Bool == true)
-        }
-
-    @Test
-    func copyToClipboard_tracksExpectedCopyEvent() {
-        let mockDrivingLicence = DrivingLicence.arrange(licenceNumber: "ABC123AE")
-        let mockAnalytics = MockAnalyticsService()
-        let sut = DrivingLicenceSummaryViewModel(
-            drivingLicence: mockDrivingLicence ,
-            statusBuilder: MockLicenceStatusViewModelBuilder(),
-            openURLAction: { _,_ in},
-            menuSelectionAction: { _ in },
-            copyToClipboardAction: { _ in },
-            analyticsService: mockAnalytics
-        )
-        sut.copyToClipboard()
-
-        #expect(mockAnalytics._trackedEvents.count == 1)
-
-        guard let event = mockAnalytics._trackedEvents.first else {
-            return
-        }
-
-        #expect(event.name == "Function")
-        #expect(event.params?["text"] as? String == "Copy to clipboard")
-        #expect(event.params?["type"] as? String == "Menu")
-        #expect(event.params?["section"] as? String == "Driver account")
-        #expect(event.params?["action"] as? String == "Copy")
-    }
+//        @Test
+//        func openUrl_replaceLicence_tracksExpectedNavigationEvent() {
+//            let mockDrivingLicence = DrivingLicence.arrange(licenceNumber: "ABC123AE")
+//            let mockAnalytics = MockAnalyticsService()
+//            let sut = DrivingLicenceSummaryViewModel(
+//                drivingLicence: mockDrivingLicence,
+//                statusBuilder: MockLicenceStatusViewModelBuilder(),
+//                openURLAction: { _,_ in},
+//                menuSelectionAction: { _ in },
+//                copyToClipboardAction: { _ in },
+//                analyticsService: mockAnalytics
+//            )
+//    
+//            sut.openUrl(options: .replaceLicence)
+//    
+//            #expect(mockAnalytics._trackedEvents.count == 1)
+//    
+//            guard let event = mockAnalytics._trackedEvents.first else {
+//                return
+//            }
+//    
+//            #expect(event.name == "Navigation")
+//            #expect(event.params?["text"] as? String == "Replace licence")
+//            #expect(event.params?["type"] as? String == "Menu")
+//            #expect(event.params?["external"] as? Bool == true)
+//        }
+//
+//    @Test
+//    func copyToClipboard_tracksExpectedCopyEvent() {
+//        let mockDrivingLicence = DrivingLicence.arrange(licenceNumber: "ABC123AE")
+//        let mockAnalytics = MockAnalyticsService()
+//        let sut = DrivingLicenceSummaryViewModel(
+//            drivingLicence: mockDrivingLicence ,
+//            statusBuilder: MockLicenceStatusViewModelBuilder(),
+//            openURLAction: { _,_ in},
+//            menuSelectionAction: { _ in },
+//            copyToClipboardAction: { _ in },
+//            analyticsService: mockAnalytics
+//        )
+//        sut.copyToClipboard()
+//
+//        #expect(mockAnalytics._trackedEvents.count == 1)
+//
+//        guard let event = mockAnalytics._trackedEvents.first else {
+//            return
+//        }
+//
+//        #expect(event.name == "Function")
+//        #expect(event.params?["text"] as? String == "Copy to clipboard")
+//        #expect(event.params?["type"] as? String == "Menu")
+//        #expect(event.params?["section"] as? String == "Driver account")
+//        #expect(event.params?["action"] as? String == "Copy")
+//    }
 
     @Test
     func changeAddress_returnsCorrectUrlAndTitle() {
@@ -128,40 +128,40 @@ struct DrivingLicenceSummaryViewModelTests {
         #expect(actualUrl == expectedUrl)
         #expect(actualTitle == "Change name or gender")
     }
-
-    @Test
-    func init_formatsLicenceTypeCorrectly() {
-        let mockDrivingLicence = DrivingLicence.arrange(licenceType: "Provisional")
-        let sut = DrivingLicenceSummaryViewModel(
-            drivingLicence: mockDrivingLicence,
-            statusBuilder: MockLicenceStatusViewModelBuilder(),
-            openURLAction: { _, _ in },
-            menuSelectionAction: {_ in },
-            copyToClipboardAction: {_ in },
-            analyticsService: MockAnalyticsService()
-        )
-        let expectedLicenceType = String.localizedStringWithFormat(
-            String.dvla.localized("licenceType"),
-            "Provisional")
-        #expect(sut.licenceType == expectedLicenceType)
-    }
-
-    @Test
-    func init_formatsFullNameCorrectly() {
-        let mockDrivingLicence = DrivingLicence.arrange(
-            driverTitle: "MR",
-            driverFirstNames: "JOE GEORGE",
-            driverLastName: "BLOGGS"
-        )
-        let sut = DrivingLicenceSummaryViewModel(
-            drivingLicence: mockDrivingLicence,
-            statusBuilder: MockLicenceStatusViewModelBuilder(),
-            openURLAction: { _, _ in },
-            menuSelectionAction: {_ in },
-            copyToClipboardAction: {_ in },
-            analyticsService: MockAnalyticsService()
-        )
-        #expect(sut.fullName == "MR JOE GEORGE BLOGGS")
+//
+//    @Test
+//    func init_formatsLicenceTypeCorrectly() {
+//        let mockDrivingLicence = DrivingLicence.arrange(licenceType: "Provisional")
+//        let sut = DrivingLicenceSummaryViewModel(
+//            drivingLicence: mockDrivingLicence,
+//            statusBuilder: MockLicenceStatusViewModelBuilder(),
+//            openURLAction: { _, _ in },
+//            menuSelectionAction: {_ in },
+//            copyToClipboardAction: {_ in },
+//            analyticsService: MockAnalyticsService()
+//        )
+//        let expectedLicenceType = String.localizedStringWithFormat(
+//            String.dvla.localized("licenceType"),
+//            "Provisional")
+//        #expect(sut.licenceType == expectedLicenceType)
+//    }
+//
+//    @Test
+//    func init_formatsFullNameCorrectly() {
+//        let mockDrivingLicence = DrivingLicence.arrange(
+//            driverTitle: "MR",
+//            driverFirstNames: "JOE GEORGE",
+//            driverLastName: "BLOGGS"
+//        )
+//        let sut = DrivingLicenceSummaryViewModel(
+//            drivingLicence: mockDrivingLicence,
+//            statusBuilder: MockLicenceStatusViewModelBuilder(),
+//            openURLAction: { _, _ in },
+//            menuSelectionAction: {_ in },
+//            copyToClipboardAction: {_ in },
+//            analyticsService: MockAnalyticsService()
+//        )
+//        #expect(sut.fullName == "MR JOE GEORGE BLOGGS")
     }
 
 //    @Test
@@ -190,42 +190,42 @@ struct DrivingLicenceSummaryViewModelTests {
 //        #expect(sut.licenceStatusViewModel.formattedStatus == "Mock licence status")
 //    }
 
-    @Test
-    func init_formatsLicenceTypeAccessibilityLabelCorrectly() {
-        let mockDrivingLicence = DrivingLicence.arrange(licenceType: "Provisional")
-        let sut = DrivingLicenceSummaryViewModel(
-            drivingLicence: mockDrivingLicence,
-            statusBuilder: MockLicenceStatusViewModelBuilder(),
-            openURLAction: { _, _ in },
-            menuSelectionAction: {_ in },
-            copyToClipboardAction: {_ in },
-            analyticsService: MockAnalyticsService()
-        )
+//    @Test
+//    func init_formatsLicenceTypeAccessibilityLabelCorrectly() {
+//        let mockDrivingLicence = DrivingLicence.arrange(licenceType: "Provisional")
+//        let sut = DrivingLicenceSummaryViewModel(
+//            drivingLicence: mockDrivingLicence,
+//            statusBuilder: MockLicenceStatusViewModelBuilder(),
+//            openURLAction: { _, _ in },
+//            menuSelectionAction: {_ in },
+//            copyToClipboardAction: {_ in },
+//            analyticsService: MockAnalyticsService()
+//        )
+//
+//        let expectedAccessibilityLabel = String.localizedStringWithFormat(
+//            String.dvla.localized("licenceTypeAccessibilityLabel"),
+//            "Provisional licence"
+//        )
+//        #expect(sut.licenceTypeAccessibilityLabel == expectedAccessibilityLabel)
+//    }
 
-        let expectedAccessibilityLabel = String.localizedStringWithFormat(
-            String.dvla.localized("licenceTypeAccessibilityLabel"),
-            "Provisional licence"
-        )
-        #expect(sut.licenceTypeAccessibilityLabel == expectedAccessibilityLabel)
-    }
-
-    @Test
-    func init_formatsAddressAccessibilityLabelCorrectly() {
-        let mockAddress = "1 LEANDER DRIVE\nCASTLETON\nOL11 4AB"
-        let mockDrivingLicence = DrivingLicence.arrange(driverFullAddress: mockAddress)
-        let sut = DrivingLicenceSummaryViewModel(
-            drivingLicence: mockDrivingLicence,
-            statusBuilder: MockLicenceStatusViewModelBuilder(),
-            openURLAction: { _, _ in },
-            menuSelectionAction: {_ in },
-            copyToClipboardAction: {_ in },
-            analyticsService: MockAnalyticsService()
-        )
-
-        let expectedAccessibilityLabel = String.localizedStringWithFormat(
-            String.dvla.localized("licenceAddressAccessibilityLabel"),
-            "1 LEANDER DRIVE, CASTLETON, OL11 4AB"
-        )
-        #expect(sut.addressAccessibilityLabel == expectedAccessibilityLabel)
-    }
+//    @Test
+//    func init_formatsAddressAccessibilityLabelCorrectly() {
+//        let mockAddress = "1 LEANDER DRIVE\nCASTLETON\nOL11 4AB"
+//        let mockDrivingLicence = DrivingLicence.arrange(driverFullAddress: mockAddress)
+//        let sut = DrivingLicenceSummaryViewModel(
+//            drivingLicence: mockDrivingLicence,
+//            statusBuilder: MockLicenceStatusViewModelBuilder(),
+//            openURLAction: { _, _ in },
+//            menuSelectionAction: {_ in },
+//            copyToClipboardAction: {_ in },
+//            analyticsService: MockAnalyticsService()
+//        )
+//
+//        let expectedAccessibilityLabel = String.localizedStringWithFormat(
+//            String.dvla.localized("licenceAddressAccessibilityLabel"),
+//            "1 LEANDER DRIVE, CASTLETON, OL11 4AB"
+//        )
+//        #expect(sut.addressAccessibilityLabel == expectedAccessibilityLabel)
+//    }
 }
