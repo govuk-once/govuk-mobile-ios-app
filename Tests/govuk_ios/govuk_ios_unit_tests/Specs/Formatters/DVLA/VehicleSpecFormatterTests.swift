100a7b42841d1531c50e9328d788cca5a862b24c
Krzysztof.Skierniewski <k.skierniewski@gmail.com>
2026-07-31 10:41:03 +0100

    update engine size formatting, litres -> cc

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/Formatters/DVLA/VehicleSpecFormatterTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Formatters/DVLA/VehicleSpecFormatterTests.swift
@@ -168,7 +168,7 @@ struct VehicleSpecFormatterTests {
     @Test
     func formatEngineSize_engineCapacityLessThan1000_returnsExpectedResult() {
         let result = sut.formatEngineSize(from: 750)
-        let expectedString = String(localized: .DVLA.engineCapacityCc(capacity: 750))
+        let expectedString = String(localized: .DVLA.engineCapacityCc(capacity: "750"))
         #expect(result.displayValue == expectedString)
         #expect(result.accessibilityLabel == expectedString)
 
@@ -177,29 +177,16 @@ struct VehicleSpecFormatterTests {
     @Test
     func formatEngineSize_engineCapacityEquals1000_returnsExpectedResult() {
         let result = sut.formatEngineSize(from: 1000)
-        let expectedDisplayValue = String(
-            localized: .DVLA.engineCapacityLitres(capacity: 1.0)
-        )
-        #expect(result.displayValue == expectedDisplayValue)
 
-        let expectedAccessibilityLabel = String(
-            localized: .DVLA.engineCapacityLitreAccessibilityLabel(capacity: 1.0)
-        )
-        #expect(result.accessibilityLabel == expectedAccessibilityLabel)
+        #expect(result.displayValue == "1000cc")
+        #expect(result.accessibilityLabel == "1,000cc")
     }
 
     @Test
     func formatEngineSize_engineCapacityGreaterThan1000_returnsExpectedResult() {
         let result = sut.formatEngineSize(from: 2495)
-        let expectedDisplayValue = String(
-            localized: .DVLA.engineCapacityLitres(capacity: 2.5)
-        )
-        #expect(result.displayValue == expectedDisplayValue)
-
-        let expectedAccessibilityLabel = String(
-            localized: .DVLA.engineCapacityLitresAccessibilityLabel(capacity: 2.5)
-        )
-        #expect(result.accessibilityLabel == expectedAccessibilityLabel)
+        #expect(result.displayValue == "2495cc")
+        #expect(result.accessibilityLabel == "2,495cc")
     }
 }
 
