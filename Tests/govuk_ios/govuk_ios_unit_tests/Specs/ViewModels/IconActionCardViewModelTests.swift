dfcfd7cb9e17ebd3490c926337199b12f53d553d
Bradley <119849471+BradleyR-Apadmi@users.noreply.github.com>
2026-09-08 15:00:05 +0100

    fix: NOT-506 improve IconActionCardView voiceover accessibility (#317)

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewModels/IconActionCardViewModelTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewModels/IconActionCardViewModelTests.swift
@@ -38,40 +38,4 @@ struct IconActionCardViewModelTests {
 
         #expect(didCallAction)
     }
-
-    @Test
-    func accessibilityLabel_includesTitleAndDescription_whenBothPresent() {
-        let sut = IconActionCardViewModel(
-            iconName: "plus.circle",
-            title: "Title",
-            description: "Description",
-            action: {}
-        )
-
-        #expect(sut.accessibilityLabel == "Title, Description")
-    }
-
-    @Test
-    func accessibilityLabel_includesOnlyDescription_whenTitleIsNil() {
-        let sut = IconActionCardViewModel(
-            iconName: "plus.circle",
-            title: nil,
-            description: "Description",
-            action: {}
-        )
-
-        #expect(sut.accessibilityLabel == "Description")
-    }
-
-    @Test
-    func accessibilityLabel_isEmpty_whenTitleAndDescriptionAreNil() {
-        let sut = IconActionCardViewModel(
-            iconName: "plus.circle",
-            title: nil,
-            description: nil,
-            action: {}
-        )
-
-        #expect(sut.accessibilityLabel == "")
-    }
 }
