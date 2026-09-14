6c15ccc417625f0365214530d93cb44185774694
KrishnaK <krishna.kotecha@digital.cabinet-office.gov.uk>
2026-09-14 12:01:26 +0100

    Changes for test/demo purposes

--- Production/govuk_ios/ServiceClients/DVLAServiceClient.swift
+++ Production/govuk_ios/ServiceClients/DVLAServiceClient.swift
@@ -61,6 +61,22 @@ class DVLAServiceClient: DVLAServiceClientInterface {
             apiServiceClient.send(
                 request: request,
                 completion: {
+                    let result: Result<T, DVLAError> = self.mapResult($0)
+                    continuation.resume(
+                        returning: result
+                    )
+                }
+            )
+        }
+    }
+
+    private func origPerformRequest<T: Decodable>(
+        _ request: GOVRequest
+    ) async -> Result<T, DVLAError> {
+        await withCheckedContinuation { continuation in
+            apiServiceClient.send(
+                request: request,
+                completion: {
                     continuation.resume(
                         returning: self.mapResult($0)
                     )
@@ -69,25 +85,28 @@ class DVLAServiceClient: DVLAServiceClientInterface {
         }
     }
 
-    private func mapResult<T: Decodable>(
-        _ result: NetworkResult<Data>
-    ) -> Result<T, DVLAError> {
-        return result.mapError { error in
-            let nsError = (error as NSError)
-            if nsError.code == NSURLErrorNotConnectedToInternet {
-                return DVLAError.networkUnavailable
-            } else {
-                return (error as? DVLAError) ?? DVLAError.apiUnavailable
-            }
-        }.flatMap {
-            do {
-                let response = try decoder.decode(T.self, from: $0)
-                return .success(response)
-            } catch {
-                return .failure(DVLAError.decodingError)
-            }
-        }
-    }
+// KK_TODO: uncomment the following method
+//       currently replaced in an extension below for test/demo purposes
+
+//    private func mapResult<T: Decodable>(
+//        _ result: NetworkResult<Data>
+//    ) -> Result<T, DVLAError> {
+//        return result.mapError { error in
+//            let nsError = (error as NSError)
+//            if nsError.code == NSURLErrorNotConnectedToInternet {
+//                return DVLAError.networkUnavailable
+//            } else {
+//                return (error as? DVLAError) ?? DVLAError.apiUnavailable
+//            }
+//        }.flatMap {
+//            do {
+//                let response = try decoder.decode(T.self, from: $0)
+//                return .success(response)
+//            } catch {
+//                return .failure(DVLAError.decodingError)
+//            }
+//        }
+//    }
 
     private lazy var decoder: JSONDecoder = {
         let decoder = JSONDecoder()
@@ -112,4 +131,85 @@ class DVLAServiceClient: DVLAServiceClientInterface {
         }
         return decoder
     }()
+}
+
+// KK_TODO: Nuke all of the following
+// MARK: - Code for Testing and dev
+extension DVLAServiceClient {
+    // KK_TODO: delete this and uncomment the real method in the main class definition
+    private func mapResult<T: Decodable>(
+        _ result: NetworkResult<Data>
+    ) -> Result<T, DVLAError> {
+        if T.self == CustomerVehicles.self {
+            // return my own govuk_ios.CustomerVehicles
+            let fauxResponse = CustomerVehicles(customerVehicles: testData)
+            if let genericFauxResponse = fauxResponse as? T {
+                return .success(genericFauxResponse)
+            }
+        }
+
+        return result.mapError { error in
+            let nsError = (error as NSError)
+            if nsError.code == NSURLErrorNotConnectedToInternet {
+                return DVLAError.networkUnavailable
+            } else {
+                return (error as? DVLAError) ?? DVLAError.apiUnavailable
+            }
+        }.flatMap {
+            do {
+                let response = try decoder.decode(T.self, from: $0)
+                return .success(response)
+            } catch {
+                return .failure(DVLAError.decodingError)
+            }
+        }
+    }
+
+    public static func arrange(_ string: String = "01/02/1993",
+                               format: String = "dd/MM/yyyy") -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.date(from: string)!
+    }
+
+    private var testData: [CustomerVehicles.Vehicle] {
+        [
+            // expired | untaxed
+            CustomerVehicles.Vehicle(vehicleId: 102088978,
+                                     registrationNumber: "VX58 ABK",
+                                     make: "VAUXHALL",
+                                     model: "WYVERN",
+                                     motStatus: "No details held by DVLA",
+                                     taxStatus: .untaxed,
+                                     dateOfLiability: nil,
+                                     sornStart: nil,
+                                     taxedUntil: DVLAServiceClient.arrange("12/12/2026"),
+                                     motExpiryDate: DVLAServiceClient.arrange("12/12/2030"),
+                                     currentLicencePaymentMethod: nil),
+            // not taxed for road use: not needed | no tax to pay
+            CustomerVehicles.Vehicle(vehicleId: 102088979,
+                                     registrationNumber: "VX58 ABK",
+                                     make: "VAUXHALL",
+                                     model: "WYVERN",
+                                     motStatus: "No details held by DVLA",
+                                     taxStatus: .notTaxedForOnRoadUse,
+                                     dateOfLiability: nil,
+                                     sornStart: nil,
+                                     taxedUntil: nil,
+                                     motExpiryDate: nil,
+                                     currentLicencePaymentMethod: nil),
+            // unknown | not found -  contact dvla
+            CustomerVehicles.Vehicle(vehicleId: 102088981,
+                                     registrationNumber: "VX58 ABK",
+                                     make: "VAUXHALL",
+                                     model: "WYVERN",
+                                     motStatus: "No details held by DVLA",
+                                     taxStatus: nil,
+                                     dateOfLiability: nil,
+                                     sornStart: nil,
+                                     taxedUntil: nil,
+                                     motExpiryDate: nil,
+                                     currentLicencePaymentMethod: nil),
+        ]
+    }
 }
