03f732dcd29a4958af4f08e36d04dd429d4cda3b
KrishnaK <krishna.kotecha@digital.cabinet-office.gov.uk>
2026-09-14 12:03:49 +0100

    `ValidityStatusView` - changes to support `StatusInformation`, beginning to eliminate overloaded state/bugs and adding Previews

--- Production/govuk_ios/Views/DVLA/ValidityStatusView.swift
+++ Production/govuk_ios/Views/DVLA/ValidityStatusView.swift
@@ -1,11 +1,27 @@
 import SwiftUI
 import GovKitUI
 
+///
+/// This view conditionally renders UI elements based on the values in `ValidityStatusViewModel`.
+///
+/// Optional values that are `nil` don't get displayed.
+/// Old/existing way: Rendering of status information is determined by `ValidityStatusViewModel.formattedStatus` being a non-empty string.
+/// New approach: we will display status information from `ValidityStatusViewModel.statusInformation`
+///
 struct ValidityStatusView: View {
     private static let iconSize: CGFloat = 36
     private static let standardPadding: CGFloat = 16.0
 
     let viewModel: ValidityStatusViewModel
+
+    @ViewBuilder
+    private var formattedStatusIfNotEmpty: some View {
+        if !viewModel.formattedStatus.isEmpty {
+            Text(viewModel.formattedStatus)
+                .multilineTextAlignment(.leading)
+                .accessibilityLabel(viewModel.statusAccessibilityLabel ?? "")
+        }
+    }
 
     var body: some View {
         VStack(spacing: 0) {
@@ -17,7 +33,12 @@ struct ValidityStatusView: View {
                             .multilineTextAlignment(.leading)
                             .accessibilityAddTraits(.isHeader)
                     }
-                    statusTextView
+
+                    formattedStatusIfNotEmpty
+
+                    if let statusInformation = viewModel.statusInformation {
+                        StatusRowView(status: statusInformation)
+                    }
                 }
                 Spacer()
                 if let iconName = viewModel.iconName {
@@ -59,11 +80,11 @@ struct ValidityStatusView: View {
     }
 }
 
-
 #Preview {
     let viewModel = ValidityStatusViewModel(
         title: nil,
         formattedStatus: "Expired 24 April 2026",
+        statusInformation: nil,
         iconName: "exclamationmark.triangle.fill",
         footer: "Your licence status may not update immediately when you renew it",
         buttonTitle: "Renew licence",
@@ -94,9 +115,198 @@ struct ValidityStatusView: View {
                 title: title,
                 formattedStatus: formattedStatus,
                 status: statusUnknown,
-                statusLinkAction: { openURLAction(text: title, url: url) }
+                statusLinkAction: { openURLAction(text: title, url: url) },
+                statusInformation: StatusInformation(
+                    title: formattedStatus,
+                    accessibilityLabel: nil,
+                    linkAction: { openURLAction(text: title, url: url) }
+                ),
             )
         }()
 
     ValidityStatusView(viewModel: notKnownVM)
+}
+
+#Preview("A Known status") {
+    let knownVM: ValidityStatusViewModel = {
+        let statusUnknown: TaxValidityStatus = .taxed
+        let url = URL(string: "https://www.gov.uk/contact-the-dvla")!
+
+        let title = String(localized: .DVLA.taxStatusTitle)
+        let formattedStatus = String(localized: .DVLA.noTaxToPay)
+
+        return ValidityStatusViewModel(
+            title: title,
+            formattedStatus: formattedStatus,
+            status: statusUnknown,
+            statusLinkAction: nil,
+            statusInformation: StatusInformation(
+                title: formattedStatus,
+                accessibilityLabel: nil,
+                linkAction: nil)
+        )
+    }()
+
+    ValidityStatusView(viewModel: knownVM)
+}
+
+#Preview("Both") {
+    let knownVM: ValidityStatusViewModel = {
+        let statusTaxed: TaxValidityStatus = .taxed
+        let url = URL(string: "https://www.gov.uk/contact-the-dvla")!
+
+        let title = String(localized: .DVLA.taxStatusTitle)
+        let formattedStatus = String(localized: .DVLA.noTaxToPay)
+
+        return ValidityStatusViewModel(
+            title: title,
+            formattedStatus: formattedStatus,
+            status: statusTaxed,
+            statusLinkAction: nil,
+            statusInformation: StatusInformation(
+                title: formattedStatus,
+                accessibilityLabel: nil,
+                linkAction: nil)
+        )
+    }()
+
+    let notKnownVM: ValidityStatusViewModel = {
+            let dummyOpenURLAction: (URL) -> Void = { url in print("click url: \(url)") }
+
+            func openURLAction(text: String, url: URL) {
+                dummyOpenURLAction(url)
+            }
+
+            let statusUnknown: TaxValidityStatus = .unknown
+            let url = URL(string: "https://www.gov.uk/contact-the-dvla")!
+
+            let title = String(localized: .DVLA.taxStatusTitle)
+            let formattedStatus = String(localized: .DVLA.notFoundContactDVLA)
+            let statusLinkAction = { openURLAction(text: title, url: url) }
+
+            return ValidityStatusViewModel(
+                title: title,
+                formattedStatus: formattedStatus,
+                status: statusUnknown,
+                statusLinkAction: statusLinkAction,
+                statusInformation: StatusInformation(
+                    title: formattedStatus,
+                    accessibilityLabel: nil,
+                    linkAction: statusLinkAction)
+            )
+        }()
+
+    let motViewModelWithLink =
+    ValidityStatusViewModel(
+        title: "mot title",
+        formattedStatus: "formattedStatus",
+        status: MOTValidityStatus.noResultsReturned,
+        statusLinkAction: {},
+        statusInformation: StatusInformation(
+            title: "status title - formattedStatus",
+            accessibilityLabel: nil,
+            linkAction: {}),
+        buttonTitle: "buttonTitle",
+    )
+    let motViewModelWithoutLink =
+    ValidityStatusViewModel(
+        title: "mot title",
+        formattedStatus: "formattedStatus",
+        status: MOTValidityStatus.noResultsReturned,
+        statusLinkAction: {},
+        statusInformation: StatusInformation(
+            title: "status title - formattedStatus",
+            accessibilityLabel: nil,
+            linkAction: nil),
+        buttonTitle: "buttonTitle",
+    )
+
+
+    VStack {
+        Text("known status: .taxed")
+        ValidityStatusView(viewModel: knownVM)
+
+        Text("unknown status: .unknown")
+        ValidityStatusView(viewModel: notKnownVM)
+
+        Text("MotValidity with Link...")
+        MotValidityStatusView(viewModel: motViewModelWithLink)
+
+        Text("MotValidity without Link...")
+        MotValidityStatusView(viewModel: motViewModelWithoutLink)
+
+        Text("ValidityStatus without Link...")
+        ValidityStatusView(viewModel: motViewModelWithoutLink)
+    }
+}
+
+
+#Preview("formattedStatus Lab") {
+    let formattedStatus = String(localized: .DVLA.untaxed)
+    let buttonTitle = String(localized: .DVLA.renewTaxButtonTitle)
+    let defaultDvlaTaxVehicleUrl: URL = URL(
+        string: "https://www.gov.uk/vehicle-tax"
+    )!
+    let buttonURL =  defaultDvlaTaxVehicleUrl
+    let viewModel = ValidityStatusViewModel(
+        title: String(localized: .DVLA.taxStatusTitle),
+        formattedStatus: formattedStatus,
+        statusInformation: nil,
+        iconName: "exclamationmark.triangle.fill",
+        footer: String(localized: .DVLA.renewTaxExpiringFooter),
+        buttonTitle: buttonTitle,
+        buttonAction: { print(buttonURL)}
+    )
+    ValidityStatusView(viewModel: viewModel)
+}
+
+#Preview("formattedStatus 2 Lab") {
+    let formattedStatus = String(localized: .DVLA.untaxed)
+    let buttonTitle = String(localized: .DVLA.renewTaxButtonTitle)
+    let defaultDvlaTaxVehicleUrl: URL = URL(
+        string: "https://www.gov.uk/vehicle-tax"
+    )!
+    let buttonURL =  defaultDvlaTaxVehicleUrl
+//    let statusInformation = StatusInformation(
+//        title: formattedStatus,
+//        accessibilityLabel: nil,
+//        linkAction: nil
+//    )
+    let statusInformation: StatusInformation? = nil
+    let viewModel = ValidityStatusViewModel(
+        title: String(localized: .DVLA.taxStatusTitle),
+        formattedStatus: formattedStatus,
+        statusInformation: statusInformation,
+        iconName: "exclamationmark.triangle.fill",
+        footer: String(localized: .DVLA.renewTaxExpiringFooter),
+        buttonTitle: buttonTitle,
+        buttonAction: { print(buttonURL)}
+    )
+    ValidityStatusView(viewModel: viewModel)
+}
+
+#Preview("Empty formattedStatus") {
+//    let formattedStatus = ""
+    let formattedStatus = String(localized: .DVLA.untaxed)
+    let buttonTitle = String(localized: .DVLA.renewTaxButtonTitle)
+    let defaultDvlaTaxVehicleUrl: URL = URL(
+        string: "https://www.gov.uk/vehicle-tax"
+    )!
+    let buttonURL =  defaultDvlaTaxVehicleUrl
+    let statusInformation = StatusInformation(
+        title: "formattedStatus",
+        accessibilityLabel: nil,
+        linkAction: nil
+    )
+//    let statusInformation: StatusInformation? = nil
+    let viewModel = ValidityStatusViewModel(
+        title: String(localized: .DVLA.taxStatusTitle),
+        formattedStatus: formattedStatus,
+        statusInformation: statusInformation,
+        iconName: "exclamationmark.triangle.fill",
+        footer: String(localized: .DVLA.renewTaxExpiringFooter),
+        buttonTitle: buttonTitle,
+        buttonAction: { print(buttonURL)}
+    )
+    ValidityStatusView(viewModel: viewModel)
 }
