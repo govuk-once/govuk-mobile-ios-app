ece7ff917e0b943b04edea71b2cd0294f41e292d
KrishnaK <krishna.kotecha@digital.cabinet-office.gov.uk>
2026-09-23 14:03:32 +0100

    `DrivingLicenseView`: Add Previews

--- Production/govuk_ios/Views/DVLA/DrivingLicenceView.swift
+++ Production/govuk_ios/Views/DVLA/DrivingLicenceView.swift
@@ -62,3 +62,124 @@ struct DrivingLicenceView: View {
             .padding(.horizontal, 16)
     }
 }
+
+#if DEBUG
+@available(iOS 17.0, *)
+#Preview("Valid licence") {
+    @Previewable @StateObject var viewModel: DrivingLicenceViewModel = {
+        let dvlaService = MockDVLAService()
+        dvlaService._stubbedFetchDrivingLicenceResult = .success(
+            .arrange(
+                licenceType: "Full",
+                licenceNumber: "MORGA753116SM9IJ",
+                driverTitle: "MRS",
+                driverFirstNames: "SARAH",
+                driverLastName: "MORGAN",
+                driverFullAddress: "10 KING'S ROAD\nCARDIFF\nCF10 3AB",
+                tokenValidToDate: .arrange("15/06/2035"),
+                licenceStatus: .valid
+            )
+        )
+
+        return DrivingLicenceViewModel(
+            analyticsService: MockAnalyticsService(),
+            dvlaService: dvlaService,
+            configService: MockAppConfigService(),
+            openURLAction: { _ in }
+        )
+    }()
+
+    NavigationStack {
+        ScrollView {
+            DrivingLicenceView(viewModel: viewModel)
+                .padding(.vertical)
+        }
+        .background(Color(uiColor: .govUK.fills.surfaceBackground))
+        .navigationTitle("Driving licence")
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview("Expired licence") {
+    @Previewable @StateObject var viewModel: DrivingLicenceViewModel = {
+        let dvlaService = MockDVLAService()
+        dvlaService._stubbedFetchDrivingLicenceResult = .success(
+            .arrange(
+                licenceNumber: "JONES810200HJ9NK",
+                driverTitle: "MR",
+                driverFirstNames: "HYWEL",
+                driverLastName: "JONES",
+                driverFullAddress: "42 HEOL FAWR\nSWANSEA\nSA1 5DF",
+                tokenValidToDate: .arrange("01/03/2023"),
+                licenceStatus: .expired
+            )
+        )
+
+        return DrivingLicenceViewModel(
+            analyticsService: MockAnalyticsService(),
+            dvlaService: dvlaService,
+            configService: MockAppConfigService(),
+            openURLAction: { _ in }
+        )
+    }()
+
+    NavigationStack {
+        ScrollView {
+            DrivingLicenceView(viewModel: viewModel)
+                .padding(.vertical)
+        }
+        .background(Color(uiColor: .govUK.fills.surfaceBackground))
+        .navigationTitle("Driving licence")
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview("Revoked licence") {
+    @Previewable @StateObject var viewModel: DrivingLicenceViewModel = {
+        let dvlaService = MockDVLAService()
+        dvlaService._stubbedFetchDrivingLicenceResult = .success(
+            .arrange(licenceStatus: .revoked)
+        )
+
+        return DrivingLicenceViewModel(
+            analyticsService: MockAnalyticsService(),
+            dvlaService: dvlaService,
+            configService: MockAppConfigService(),
+            openURLAction: { _ in }
+        )
+    }()
+
+    NavigationStack {
+        ScrollView {
+            DrivingLicenceView(viewModel: viewModel)
+                .padding(.vertical)
+        }
+        .background(Color(uiColor: .govUK.fills.surfaceBackground))
+        .navigationTitle("Driving licence")
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview("Error") {
+    @Previewable @StateObject var viewModel: DrivingLicenceViewModel = {
+        let dvlaService = MockDVLAService()
+        dvlaService._stubbedFetchDrivingLicenceResult = .failure(.apiUnavailable)
+
+        return DrivingLicenceViewModel(
+            analyticsService: MockAnalyticsService(),
+            dvlaService: dvlaService,
+            configService: MockAppConfigService(),
+            openURLAction: { _ in }
+        )
+    }()
+
+    NavigationStack {
+        ScrollView {
+            DrivingLicenceView(viewModel: viewModel)
+                .padding(.vertical)
+        }
+        .background(Color(uiColor: .govUK.fills.surfaceBackground))
+        .navigationTitle("Driving licence")
+    }
+}
+#endif // DEBUG
