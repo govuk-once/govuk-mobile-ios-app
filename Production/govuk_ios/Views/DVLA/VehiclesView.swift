99775773559997c1febd075b9ce10525fd49948a
KrishnaK <krishna.kotecha@digital.cabinet-office.gov.uk>
2026-09-24 10:29:11 +0100

    Extract Previews data into an extension in `Development/Support/Previews Data`

--- Production/govuk_ios/Views/DVLA/VehiclesView.swift
+++ Production/govuk_ios/Views/DVLA/VehiclesView.swift
@@ -125,37 +125,7 @@ struct VehiclesView: View {
         let dvlaService = MockDVLAService()
         dvlaService._stubbedCustomerVehiclesResult = .success(
             .arrange(
-                customerVehicles: [
-                    .arrange(
-                        vehicleId: 1,
-                        registrationNumber: "AB71 CDE",
-                        make: "MITSUBISHI",
-                        model: "MIRAGE",
-                        taxStatus: .taxed,
-                        taxedUntil: .arrange("15/09/2030"),
-                        motStatus: "Valid",
-                        motExpiryDate: .arrange("15/09/2030")
-                    ),
-                    .arrange(
-                        vehicleId: 2,
-                        registrationNumber: "XY19 ZAB",
-                        make: "LAND ROVER",
-                        model: "RANGE ROVER SPORT",
-                        taxStatus: .sorn,
-                        motStatus: "Not valid",
-                        motExpiryDate: .arrange("01/03/2024"),
-                        sornStart: .arrange("01/01/2025")
-                    ),
-                    .arrange(
-                        vehicleId: 3,
-                        registrationNumber: "MN65 PQR",
-                        make: "VOLKSWAGEN",
-                        model: "GOLF",
-                        taxStatus: .untaxed,
-                        motStatus: "Valid",
-                        motExpiryDate: .arrange("20/11/2030")
-                    )
-                ]
+                customerVehicles: CustomerVehicles.Vehicle.PreviewsData.collection
             )
         )
 
@@ -188,18 +158,7 @@ struct VehiclesView: View {
         let dvlaService = MockDVLAService()
         dvlaService._stubbedCustomerVehiclesResult = .success(
             .arrange(
-                customerVehicles: [
-                    .arrange(
-                        vehicleId: 1,
-                        registrationNumber: "FG23 HIJ",
-                        make: "TESLA",
-                        model: "MODEL 3",
-                        taxStatus: .taxed,
-                        taxedUntil: .arrange("01/04/2031"),
-                        motStatus: "Valid",
-                        motExpiryDate: .arrange("01/04/2031")
-                    )
-                ]
+                customerVehicles: [CustomerVehicles.Vehicle.PreviewsData.teslaVehicle]
             )
         )
 
